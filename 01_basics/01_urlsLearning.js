b02ff8a3df2e7d4fe0a0dfbfda3d31711b530f1d
Vishal Yadav <nishayadav1703@gmail.com>
2024-04-29 21:41:39 +0530

    function js

--- 01_basics/01_urlsLearning.js
+++ 01_basics/01_urlsLearning.js
@@ -2,3 +2,6 @@
 // https://www.youtube.com/playlist?list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37
 
 // run command to see result into the vs terminal // node 01_basics/consoleTable.js
+
+// JSON API url
+// https://randomuser.me/
