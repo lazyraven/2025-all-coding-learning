79cc97be5a6e644f4f0735d2c1e8f76a6769ce8c
Vishal Yadav <vishalyadaviit@gmail.com>
2024-04-29 16:10:49 +0530

    String, array, objects, date, stack-heap & deep shallow copy

---
+++ 01_basics/01_urlsLearning.js
@@ -0,0 +1,4 @@
+// https://github.com/hiteshchoudhary/js-hindi-youtube
+// https://www.youtube.com/playlist?list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37
+
+// run command to see result into the vs terminal // node 01_basics/consoleTable.js
